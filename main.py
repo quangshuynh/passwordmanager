ec6683de32557a16507028d30d4d0dad9c8b7abd
kf5601 <kf5601@rit.edu>
2024-07-24 22:44:01 -0400

    creating a new class that handles file edits

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Password Manager Project
 author: Quang Huynh, Kai Fan
 """
 import os
-# used to create a folder that holds all the files
+# used to create and edit files and folders
 
 from cryptography.fernet import Fernet
 # used to encrypt passwords
@@ -21,18 +21,25 @@ class PasswordManager:
         with open(path, "wb") as file:
             file.write(self.key)
 
-class FileEdits:
-    def create_file(fileName):
-        print('Creating a new file: "' + fileName + '"')
+class FileEditor:
+    def create_text_file(fileName:str)->None: #this method overrides file with same name!
+        print('Creating a new text file: "' + fileName + '"')
         with open(fileName + ".txt", "w") as newFile:
             pass
-    def delete_file(fileName):
-        print("in development")
+    def delete_file(fileName:str)->None: #careful with this method, it deletes things other than txt file
+        print('Deleting file: "' + fileName + '"')
+        try:
+            os.remove(fileName)
+        except PermissionError:
+            print("Permission denied")
+    #def create_new_user(): ###Waiting for response KF
+        #with open()
 
-#testing create_file
-FileEdits.create_file("poop")
+#testing create_text_file
+FileEditor.create_text_file("poop")
 
-
+#testing delete_file
+FileEditor.delete_file("poop.txt")
 
 # debugging
 pm = PasswordManager()
